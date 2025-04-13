579e316ccc2157f2a9ffd052f68bf27b2e18284b
Sarobid <mbolasaroubidy@gmail.com>
2025-04-13 18:41:27 +0200

    update and delete grades

--- routes/grades.js
+++ routes/grades.js
@@ -29,5 +29,44 @@ function create(req, res) {
         res.status(400).send('cant post grade ', err.message);
     });
 }
+function update(req, res) {
+    const gradeId = req.params.id;
 
-module.exports = {getAll, create};
+    Grade.findByIdAndUpdate(
+        gradeId,
+        {
+            student: req.body.student,
+            course: req.body.course,
+            grade: req.body.grade,
+            date: req.body.date
+        },
+        { new: true, runValidators: true }
+    )
+    .then((updatedGrade) => {
+        if (!updatedGrade) {
+            return res.status(404).send({ message: 'Grade not found' });
+        }
+        res.send(updatedGrade);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(400).send({ message: 'Unable to update grade', error: err.message });
+    });
+}
+function deleteGrade(req, res) {
+    const gradeId = req.params.id;
+
+    Grade.findByIdAndDelete(gradeId)
+        .then((deletedGrade) => {
+            if (!deletedGrade) {
+                return res.status(404).send({ message: 'Grade not found' });
+            }
+            res.send({ message: `Grade with id ${gradeId} deleted successfully.` });
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(400).send({ message: 'Unable to delete grade', error: err.message });
+        });
+}
+
+module.exports = {getAll, create, update, deleteGrade};
