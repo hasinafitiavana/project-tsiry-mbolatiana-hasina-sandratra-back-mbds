6cf98d459e966268e769abe279b9462323609bad
Sarobid <mbolasaroubidy@gmail.com>
2025-05-01 21:36:06 +0300

    implement list users

--- routes/users.js
+++ routes/users.js
@@ -52,6 +52,20 @@ router.post("/refresh-token", async function refreshToken(req, res) {
     }
 })
 
+router.get("/", async function getAllUsers(req, res) {
+    try {
+        const users = await User.find({});
+        const data = users.map((user) => {
+            const { password,roles, ...u } = user.toObject();
+            return u;
+        });
+        res.status(200).json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+})
+
 
 
 module.exports = router;
